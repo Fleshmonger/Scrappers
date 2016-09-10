cbd3232c4a678ac92fa75e0d6c398316cb307e92
Fleshmonger <fleshmonger@gmail.com>
2016-09-10 18:02:02 +0200

    Moved Player controls to separate class PlayerController

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public Player player;
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = Vector3.zero;
+        if (InputMoveRight())
+        {
+            direction += Vector3.right;
+        }
+        if (InputMoveLeft())
+        {
+            direction += Vector3.left;
+        }
+        if (InputMoveUp())
+        {
+            direction += Vector3.up;
+        }
+        if (InputMoveDown())
+        {
+            direction += Vector3.down;
+        }
+        player.Move(direction);
+
+        if (InputShoot() && player.weapon)
+        {
+            player.weapon.Attack(player.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), player.faction);
+        }
+    }
+
+    private bool InputMoveRight()
+    {
+        return Input.GetKey(KeyCode.D);
+    }
+
+    private bool InputMoveLeft()
+    {
+        return Input.GetKey(KeyCode.A);
+    }
+
+    private bool InputMoveUp()
+    {
+        return Input.GetKey(KeyCode.W);
+    }
+
+    private bool InputMoveDown()
+    {
+        return Input.GetKey(KeyCode.S);
+    }
+
+    private bool InputShoot()
+    {
+        return Input.GetMouseButton(0);
+    }
+}
